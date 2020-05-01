7231d3c83348f47627217fc7d274d0bd58b28536
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-01 21:42:42 +0400

    deleted P tags, added header for Latest Data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import CountrySelector from "./components/Selectors/CountrySelector";
 import GlobalData from "./components/AllData/GlobalData";
 import CountryData from "./components/AllData/CountryData";
-
 function App() {
   const [country, setCountry] = useState("");
 
@@ -15,11 +14,8 @@ function App() {
     <div>
       <h1 className="tc ma5">COVID-19 Tracker</h1>
       <CountrySelector handleCountryChange={handleCountryChange} />
+      <h3 className="dark-gray tc">Latest Data</h3>
       <GlobalData countrySlug={country} />
-      <p className="mid-gray tc mt5">For detailed data choose country</p>
-      <p className="mid-gray tc">
-        Some countries may not have detailed information
-      </p>
       <CountryData countrySlug={country} />
     </div>
   );
