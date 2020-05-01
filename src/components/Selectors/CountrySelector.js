1976682d30b6513b5b23c81036993a8b92b32aa0
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-01 16:04:11 +0400

    added country options, setting slug state used as a option value for selected country. added handleCountryChange property

--- src/components/Selectors/CountrySelector.js
+++ src/components/Selectors/CountrySelector.js
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function CountrySelector() {
+function CountrySelector({ handleCountryChange }) {
   const [data, setdata] = useState([]);
-  const [slug, setSlug] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://api.covid19api.com/countries")
       .then((response) => {
-        const slugs = response.data.map((obj) => {
-          return obj.Slug;
-        });
         setdata(response.data);
-        setSlug(slugs);
       })
       .catch((err) => {
         console.error("[CountrySelector.jsx]", err.message);
       });
-  }, []);
+  }, [setdata]);
 
   const countries = data.map((obj) => {
-    return <option key={obj.ISO2}>{obj.Country}</option>;
+    return (
+      <option key={obj.ISO2} value={obj.Slug}>
+        {obj.Country}
+      </option>
+    );
   });
 
   return (
     <div className="tc ma5 ">
-      <select>
+      <select
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
         <option value="Global">Global</option>
         {countries}
       </select>
