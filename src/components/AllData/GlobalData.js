c82482dda444e5f02acf58ff5ed14d4fb3f2805d
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-01 16:06:25 +0400

    added CountryLatestData state set as filtered value from requested api, giving card component additional properties

--- src/components/AllData/GlobalData.js
+++ src/components/AllData/GlobalData.js
@@ -2,27 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "../Cards/Card";
 
-function GlobalData(props) {
+function GlobalData({ countrySlug }) {
   const [GlobalData, setGlobalData] = useState([]);
+  const [CountryLatestData, setCountryLatestData] = useState(null);
   const [date, setCurrentDate] = useState("");
 
   useEffect(() => {
     axios
       .get("https://api.covid19api.com/summary")
       .then((response) => {
-        console.log(response.data.Global);
-        console.log(response.data.Date);
         setGlobalData(response.data.Global);
         setCurrentDate(response.data.Date);
+        const countryObj = response.data.Countries.filter((obj) => {
+          return obj.Slug === countrySlug;
+        });
+        console.log(countryObj);
+        if (countrySlug && countrySlug !== "Global") {
+          setCountryLatestData(countryObj);
+        }
       })
       .catch((err) => {
         console.error("[GlobalData.jsx]", err.message);
       });
-  }, []);
+  }, [countrySlug]);
 
   return (
     <div>
-      <Card GlobalData={GlobalData} date={date} />
+      <Card
+        selected={countrySlug}
+        date={date}
+        GlobalData={GlobalData}
+        CountryLatestData={CountryLatestData}
+      />
     </div>
   );
 }
