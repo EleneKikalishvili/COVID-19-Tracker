a10dcdfc99a3d58038e6665ce3b27f47dbbe1814
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-01 19:54:18 +0400

    added CountryData component and P tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CountrySelector from "./components/Selectors/CountrySelector";
 import GlobalData from "./components/AllData/GlobalData";
+import CountryData from "./components/AllData/CountryData";
 
 function App() {
   const [country, setCountry] = useState("");
@@ -12,8 +13,14 @@ function App() {
 
   return (
     <div>
+      <h1 className="tc ma5">COVID-19 Tracker</h1>
       <CountrySelector handleCountryChange={handleCountryChange} />
       <GlobalData countrySlug={country} />
+      <p className="mid-gray tc mt5">For detailed data choose country</p>
+      <p className="mid-gray tc">
+        Some countries may not have detailed information
+      </p>
+      <CountryData countrySlug={country} />
     </div>
   );
 }
