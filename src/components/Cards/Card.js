bf83272be556ac315f14855a1dc51efdfa2a8cdf
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-01 16:17:43 +0400

    added CountryLatesData and selected properties. using if else statement for setting data value according to requested data.

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,35 +1,33 @@
 import React from "react";
 
-function Card({
-  date,
-  GlobalData: {
-    NewConfirmed,
-    TotalConfirmed,
-    NewDeaths,
-    TotalDeaths,
-    NewRecovered,
-    TotalRecovered,
-  },
-}) {
+function Card({ date, GlobalData, CountryLatestData, selected }) {
+  let data = null;
+
+  if (CountryLatestData && selected !== "Global") {
+    data = CountryLatestData[0];
+  } else {
+    data = GlobalData;
+  }
+
   return (
     <div className="tc">
       <div className="bg-light-blue ma1 pa3 shadow-hover br2 dib w-20 tc">
         <h1>Infected</h1>
         <p> {date}</p>
-        <p> New Confirmed Cases: {NewConfirmed}</p>
-        <p>Total Confirmed Cases: {TotalConfirmed}</p>
+        <p> New Confirmed Cases: {data.NewConfirmed}</p>
+        <p>Total Confirmed Cases: {data.TotalConfirmed}</p>
       </div>
       <div className="bg-light-green ma1 pa3 shadow-hover br2 dib w-20 tc">
         <h1>Recovered</h1>
         <p> {date}</p>
-        <p> New Recovered Cases: {NewRecovered}</p>
-        <p>Total Recovered Cases: {TotalRecovered}</p>
+        <p> New Recovered Cases: {data.NewRecovered}</p>
+        <p>Total Recovered Cases: {data.TotalRecovered}</p>
       </div>
       <div className="bg-light-pink ma1 pa3 shadow-hover br2 dib w-20 tc">
         <h1>Deaths</h1>
         <p> {date}</p>
-        <p> New Death Cases: {NewDeaths}</p>
-        <p>Total Death Cases: {TotalDeaths}</p>
+        <p> New Death Cases: {data.NewDeaths}</p>
+        <p>Total Death Cases: {data.TotalDeaths}</p>
       </div>
     </div>
   );
